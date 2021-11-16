563edc6b41ec69c10f8ceacb7b6cd9626184b920
Subhasis Jethy <46371699+subhasisj@users.noreply.github.com>
2021-11-17 01:11:03 +0530

    Changes to Setup.py

--- setup.py
+++ setup.py
@@ -5,22 +5,21 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 ## edit below variables as per your requirements -
 REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+AUTHOR_USER_NAME = "subhasisj"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
-
+DESCRIPTION = "Describe your package here"
 
 setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    author_email="subhasis.jethy@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
     install_requires=LIST_OF_REQUIREMENTS
-)
